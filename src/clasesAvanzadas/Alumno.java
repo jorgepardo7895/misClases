11bac04833d3ca4c053176185a108d0d647ee119
jorge7895 <jorge7895@gmail.com>
2021-02-08 13:28:54 +0100

    actualizacion teclado

--- src/clasesAvanzadas/Alumno.java
+++ src/clasesAvanzadas/Alumno.java
@@ -1,22 +1,57 @@
 package clasesAvanzadas;
 
 public class Alumno {
-	public String nombreCompleto;
-	public int edad;
+	public String nombre;
+	public int telefono;
 	public String curso;
+	public String dni;
+	public String asignaturas[];
+	public float notas[];
 	
-	
-	public String getNombre() {
-		return nombreCompleto;
+	public Alumno() {
+		nombre="";
+		telefono=0;
+		curso="";
+		dni="";
+		asignaturas=new String[] {"Programación","Base de Datos","Sistemas"};
+		notas=new float[3];
 	}
-	public void setNombre(String nombreCompleto) {
-		this.nombreCompleto = nombreCompleto;
+	
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String[] getAsignaturas() {
+		return asignaturas;
+	}
+
+	public float[] getNotas() {
+		return notas;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 	public int getEdad() {
-		return edad;
+		return telefono;
 	}
 	public void setEdad(int edad) {
-		this.edad = edad;
+		this.telefono = edad;
 	}
 	public String getCurso() {
 		return curso;
--- src/clasesAvanzadas/Teclado.java
+++ src/clasesAvanzadas/Teclado.java
@@ -3,14 +3,20 @@ package clasesAvanzadas;
 import java.util.Scanner;
 
 public class Teclado {
-	Scanner sc=new Scanner(System.in);
 	
+	//atributos
+	private Scanner sc;
 	
+	//contructor por defecto
+	public Teclado() {
+		sc=new Scanner(System.in);
+	}
 	
-	public String letras(String entrada) {
-		String aux="";
+	public String letras() {
+		String aux="", entrada="";
 		int cont=entrada.length();
-		
+		System.out.println("Introduce solo palabras");
+		entrada=sc.nextLine();
 		do {
 			
 			for (int i = 0; i < entrada.length(); i++) {
@@ -30,10 +36,11 @@ public class Teclado {
 		return aux;
 	}
 	
-	public int enteros(String entrada) {
-		String aux="";
+	public int enteros() {
+		String aux="", entrada="";
 		int numero=0, cont=entrada.length();
-		
+		System.out.println("Introduce un numero entero");
+		entrada=sc.nextLine();
 		do {
 			
 			for (int i = 0; i < entrada.length(); i++) {
@@ -41,6 +48,38 @@ public class Teclado {
 					aux+=entrada.charAt(i);
 					cont--;
 				}else if(Character.isLetter(entrada.charAt(i))) {
+					System.out.println("Error, inserte un numero por favor");
+					entrada=sc.nextLine();
+					cont=entrada.length();
+					break;
+				}
+			}
+			
+		}while(cont>0);
+			
+		numero = (int)Integer.parseInt(aux);
+		return numero;
+	}
+	
+	public float decimales() {
+		String aux="", entrada="";
+		int cont=entrada.length();
+		float numero;
+		System.out.println("Introduce un numero decimal");
+		entrada=sc.nextLine();
+		do {
+			
+			for (int i = 0; i < entrada.length(); i++) {
+				if (Character.isDigit(entrada.charAt(i))||entrada.charAt(i)==','||entrada.charAt(i)=='.') {
+					if (Character.isDigit(entrada.charAt(i))||entrada.charAt(i)=='.') {
+						aux+=entrada.charAt(i);
+						cont--;
+					}else if(entrada.charAt(i)==',') {
+						aux+='.';
+						cont--;
+					}
+					
+				}else if(Character.isLetter(entrada.charAt(i))||entrada.charAt(i)!=','||entrada.charAt(i)!='.') {
 					System.out.println("Error, inserte un numero por favor");
 					entrada=sc.next();
 					cont=entrada.length();
@@ -50,7 +89,7 @@ public class Teclado {
 			
 		}while(cont>0);
 			
-		numero = (int)Integer.parseInt(aux);
+		numero = (float)Float.parseFloat(aux);
 		return numero;
 	}
 }
